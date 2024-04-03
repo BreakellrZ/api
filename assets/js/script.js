b38d732cb8d139a9123b5222be8be615bcc8b2cb
Colin Breakell <cbreakell99@gmail.com>
2024-04-03 22:06:07 +0000

    Api progress

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+const API_KEY = "tLz9kDI65h_Kko1RBlOTG2u1oTM";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data.expiry);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+
+}
+
+document.getElementById("submit").addEventListener("click", e => postForm(e)); 
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+    
+    const response = await fetch(API_URL, {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": API_KEY,
+                                 },
+                                 body: form,
+                        })
+
+                        
+
+}
